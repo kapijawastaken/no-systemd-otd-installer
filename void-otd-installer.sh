dc48196a3b49e19de77b5b9d944a931362100e78
kapija <140849011+kapijawastaken@users.noreply.github.com>
2024-12-28 22:02:18 +0100

    Add files via upload

--- void-otd-installer.sh
+++ void-otd-installer.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v0.6.5.0/opentabletdriver-0.6.5.0-x64.tar.gz
-mkdir ~/Applications
-tar xvf ./opentabletdriver-0.*.tar.gz --directory=~/Applications
-rm opentabletdriver-0.*.tar.gz
-echo "Make sure to add ~/Applications/opentabletdriver/usr/local/bin to your $PATH!"
+mkdir -p ~/Applications/
+cd ~/Applications/
+tar xvf ../Downloads/opentabletdriver-0.*.tar.gz
+rm ../Downloads/opentabletdriver-0.*.tar.gz
+echo "Make sure to add ~/Applications/opentabletdriver/usr/local/bin to your PATH environment variable!"
