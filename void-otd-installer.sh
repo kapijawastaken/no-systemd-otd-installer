b2125105d4ef6d3ddc5a7d9e2b064db080eb02ce
kapija <140849011+kapijawastaken@users.noreply.github.com>
2025-01-17 19:05:39 +0100

    Update void-otd-installer.sh

--- void-otd-installer.sh
+++ void-otd-installer.sh
@@ -1,7 +1,11 @@
 #/bin/sh
+cd ~/Downloads
 wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v0.6.5.0/opentabletdriver-0.6.5.0-x64.tar.gz
-mkdir -p ~/Applications/
+mkdir ~/Applications/
 cd ~/Applications/
 tar xvf ../Downloads/opentabletdriver-0.*.tar.gz
 rm ../Downloads/opentabletdriver-0.*.tar.gz
-echo "Make sure to add ~/Applications/opentabletdriver/usr/local/bin to your PATH environment variable!"
+sudo cp ~/Applications/opentabletdriver/etc/udev/rules.d/70-opentabletdriver.rules /usr/lib/udev/rules.d/
+sudo sv restart udevd
+sudo sed -i '1i\rmmod wacom hid_uclogic\' /etc/profile
+echo "Make sure to add '~/Applications/opentabletdriver/usr/local/bin/otd-daemon &' to whatever startup file you want!"
