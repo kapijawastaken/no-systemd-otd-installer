c1fe716637f25518c98e01a110098c81db9caa35
kapija <140849011+kapijawastaken@users.noreply.github.com>
2025-04-13 08:12:22 +0200

    Update no-systemd-otd-installer.sh

--- no-systemd-otd-installer.sh
+++ no-systemd-otd-installer.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd ~/Downloads
-wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v0.6.5.0/opentabletdriver-0.6.5.0-x64.tar.gz
+wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v0.6.5.1/opentabletdriver-0.6.5.1-x64.tar.gz
 mkdir ~/Applications/
 cd ~/Applications/
 tar xvf ../Downloads/opentabletdriver-0.*.tar.gz
 rm ../Downloads/opentabletdriver-0.*.tar.gz
 sudo cp ~/Applications/opentabletdriver/etc/udev/rules.d/70-opentabletdriver.rules /usr/lib/udev/rules.d/
-sudo sv restart udevd
+sudo udevadm control --reload-rules && udevadm trigger
 sudo sed -i '1i\rmmod wacom hid_uclogic\' /etc/profile
 echo "Make sure to add '~/Applications/opentabletdriver/usr/local/bin/otd-daemon &' to whatever startup file you want!"
