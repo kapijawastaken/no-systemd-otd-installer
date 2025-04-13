8a94251ea214be3f24b15b93aac09cc58a1c86eb
kapija <140849011+kapijawastaken@users.noreply.github.com>
2025-04-13 08:34:35 +0200

    Update no-systemd-otd-installer.sh

--- no-systemd-otd-installer.sh
+++ no-systemd-otd-installer.sh
@@ -3,3 +3,9 @@ cd ~/Downloads
 wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/download/v0.6.5.1/opentabletdriver-0.6.5.1-x64.tar.gz
 sudo tar xvf ../Downloads/opentabletdriver-0.*.tar.gz --directory=/
 sudo sed -i '1i\rmmod wacom hid_uclogic\' /etc/profile
+sudo udevadm trigger
+cd /opentabletdriver/
+sudo cp * ../
+cd /
+sudo rm -rf opentabletdriver/
+rm ~/Downloads/opentabletdriver-0.*.tar.gz
